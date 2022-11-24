ab28613d8918b61e0596eec04bf9390474fd2e99
Leonardo Xavier da Silva Moraes <moraes1@ufpr.br>
2022-11-23 22:54:21 -0300

    Fazendo tratamento de campo vazio.

--- app/src/main/java/com/example/multiactivity/MainActivity.java
+++ app/src/main/java/com/example/multiactivity/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,25 +18,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onClick(View view ){
+    public void situacaoFinal(View view ){
         EditText _nome = findViewById(R.id.nome);
         EditText _nota1 = findViewById(R.id.nota1);
         EditText _nota2 = findViewById(R.id.nota2);
         EditText _freq = findViewById(R.id.freq);
 
         String nome = _nome.getText().toString();
-        Integer nota1 = Integer.parseInt(_nota1.getText().toString());
-        Integer nota2 = Integer.parseInt(_nota2.getText().toString());
-        Integer freq = Integer.parseInt(_freq.getText().toString());
+        if (!_nota1.getText().toString().equals("") && !_nota2.getText().toString().equals("")) {
+            Integer nota1 = Integer.parseInt(_nota1.getText().toString());
+            Integer nota2 = Integer.parseInt(_nota2.getText().toString());
+            Integer freq = Integer.parseInt(_freq.getText().toString());
 
-
-        Intent it = new Intent( this, SituacaoFinal.class);
-        Bundle params = new Bundle();
-        params.putString("nome", nome);
-        params.putInt("freq", freq);
-        params.putFloat("nota1", nota1);
-        params.putFloat("nota2", nota2);
-        it.putExtras(params);
-        startActivity(it);
+            Intent it = new Intent(this, SituacaoFinal.class);
+            Bundle params = new Bundle();
+            params.putString("nome", nome);
+            params.putInt("freq", freq);
+            params.putFloat("nota1", nota1);
+            params.putFloat("nota2", nota2);
+            it.putExtras(params);
+            startActivity(it);
+        } else {
+            Toast.makeText(getApplicationContext(),"Favor informar notas.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
